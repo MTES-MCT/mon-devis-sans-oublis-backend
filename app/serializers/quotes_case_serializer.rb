62e6d5547c4120138640de08f66d9bdd7407a3cb
nicolasleger <nicolasleger@users.noreply.github.com>
2025-06-04 09:23:23 +0200

    fix: serializers

--- app/serializers/quotes_case_serializer.rb
+++ app/serializers/quotes_case_serializer.rb
@@ -2,5 +2,9 @@
 
 class QuotesCaseSerializer < BaseSerializer
   attributes :id, :reference, :status
-  has_many :quote_checks, serializer: QuoteCheckInsideCaseSerializer
+  has_many :quote_checks, serializer: QuoteCheckInsideCaseSerializer, if: :quote_checks
+
+  def quote_checks
+    object.quote_checks.order(created_at: :desc).presence
+  end
 end
