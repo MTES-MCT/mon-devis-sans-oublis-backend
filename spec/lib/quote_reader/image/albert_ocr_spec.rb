94bf01590763105a690af59472325bee886a61cb
nicolasleger <nicolasleger@users.noreply.github.com>
2025-06-06 18:29:14 +0200

    fix: OCRs

--- spec/lib/quote_reader/image/albert_ocr_spec.rb
+++ spec/lib/quote_reader/image/albert_ocr_spec.rb
@@ -3,15 +3,15 @@
 require "rails_helper"
 
 RSpec.describe QuoteReader::Image::AlbertOcr, type: :service do
+  subject(:albert_ocr) { described_class.new(quote_file.content, "image/png", quote_file:) }
+
   let(:file) { fixture_file_upload("quote_files/Devis_test.png") }
   let(:quote_file) { create(:quote_file, file:) }
 
   describe "#extract_text" do
     it "returns the content", :vcr do
       skip "Albert OCR is not working with images"
-      expect(
-        described_class.new(quote_file.content, "image/png", quote_file:).extract_text
-      ).to include("Nice")
+      expect(albert_ocr.extract_text).to include("Nice")
     end
   end
 end
