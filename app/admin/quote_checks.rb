4b5a707c74eaac3aa9fb7f8d6560abc6633c5872
nicolasleger <nicolasleger@users.noreply.github.com>
2025-06-30 11:28:19 +0200

    fix(BO): double button

--- app/admin/quote_checks.rb
+++ app/admin/quote_checks.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
+# Enure only one button display
+ActiveAdmin.register QuoteCheck do
+  action_item :recheck, only: :show do
+    link_to "Re-vérifier à nouveau", recheck_admin_quote_check_path(resource), method: :post if resource.recheckable?
+  end
+end
+
 Rails.root.glob("app/admin/quote_checks/*.rb").each { |f| require f }
