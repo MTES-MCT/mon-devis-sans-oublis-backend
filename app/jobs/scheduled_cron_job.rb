9a29ddc0781dbc68caae74caf84df76fb604381f
nicolasleger <nicolasleger@users.noreply.github.com>
2025-07-30 10:53:04 +0200

    fix: ScheduledCronJob

--- app/jobs/scheduled_cron_job.rb
+++ app/jobs/scheduled_cron_job.rb
@@ -5,19 +5,16 @@ class ScheduledCronJob < ApplicationJob
   protected
 
   def monitor_cron_job
-    crontab = Rails.application.config.good_job.dig(:cron, name.underscore.to_sym, :cron) || raise(NotImplementedError)
+    crontab = Rails.application.config.good_job.dig(:cron, self.class.name.underscore.to_sym, :cron) ||
+              raise(NotImplementedError)
 
     monitor_config = Sentry::Cron::MonitorConfig.from_crontab(crontab)
     monitor_slug = self.class.name
 
-    check_in_id = Sentry.capture_check_in(
-      monitor_slug, :in_progress, monitor_config:
-    )
+    check_in_id = Sentry.capture_check_in(monitor_slug, :in_progress, monitor_config:)
 
     yield
 
-    Sentry.capture_check_in(
-      monitor_slug, :ok, check_in_id:, monitor_config:
-    )
+    Sentry.capture_check_in(monitor_slug, :ok, check_in_id:, monitor_config:)
   end
 end
