0b50cca31f9e95656b15329a00e33addc3cf1925
nicolasleger <nicolasleger@users.noreply.github.com>
2025-07-30 10:16:39 +0200

    chore: scheduled CRON job monitoring

--- app/jobs/quote_checks_timeout_job.rb
+++ app/jobs/quote_checks_timeout_job.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 # This job checks if some QuoteChecks has timed out and processes it accordingly.
-class QuoteChecksTimeoutJob < ApplicationJob
+class QuoteChecksTimeoutJob < ScheduledCronJob
   queue_as :default
 
-  def perform
-    quote_checks = QuoteCheck.pending.order(started_at: :asc)
-    return unless quote_checks.any?
+  CRONTAB = "*/5 * * * *"
 
-    quote_checks.each { QuoteCheckTimeout.new(it).check }
+  def perform
+    monitor_cron_job do
+      quote_checks = QuoteCheck.pending.order(started_at: :asc)
+      return unless quote_checks.any?
+
+      quote_checks.each { QuoteCheckTimeout.new(it).check }
+    end
   end
 end
