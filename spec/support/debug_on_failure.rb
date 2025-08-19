a8df95f75a239479ce9d1d3457870739ec94ea61
nicolasleger <nicolasleger@users.noreply.github.com>
2025-08-19 16:33:59 +0200

    fix: lint

--- spec/support/debug_on_failure.rb
+++ spec/support/debug_on_failure.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.after do |example| # rubocop:disable Lint/Output
+  config.after do |example|
     next if ENV["CI"]
 
     if example.exception
       location = example.metadata[:location]
       file = example.metadata[:file_path]
 
-      puts "\n🔍 Test failed at: #{file}:#{location.split(':').last}"
-      puts "💡 Re-run with: rspec #{file}:#{location.split(':').last}"
+      puts "\n🔍 Test failed at: #{file}:#{location.split(':').last}" # rubocop:disable Lint/Output
+      puts "💡 Re-run with: rspec #{file}:#{location.split(':').last}" # rubocop:disable Lint/Output
 
       debugger # rubocop:disable Lint/Debugger
     end
