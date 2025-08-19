827f8d39590199607ec9de50d4cb86a823bd3c9f
nicolasleger <nicolasleger@users.noreply.github.com>
2025-08-19 14:00:39 +0200

    chore: auto debug on failure

---
+++ spec/support/debug_on_failure.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.after do |example| # rubocop:disable Lint/Output
+    next if ENV["CI"]
+
+    if example.exception
+      location = example.metadata[:location]
+      file = example.metadata[:file_path]
+
+      puts "\n🔍 Test failed at: #{file}:#{location.split(':').last}"
+      puts "💡 Re-run with: rspec #{file}:#{location.split(':').last}"
+
+      debugger # rubocop:disable Lint/Debugger
+    end
+  end
+end
