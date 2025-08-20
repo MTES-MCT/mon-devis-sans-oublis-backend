4e7892354da772c071fa17f450f97b753c34d900
nicolasleger <nicolasleger@users.noreply.github.com>
2025-08-20 19:25:00 +0200

    fix: Ademe RGE paginations

--- lib/data_ademe.rb
+++ lib/data_ademe.rb
@@ -20,13 +20,23 @@ class DataAdeme
     # Example: https://data.ademe.fr/data-fair/api/v1/datasets/liste-des-entreprises-rge-2/lines?page=1&after=1&size=12&sort=nom_entreprise&select=siret,nom_entreprise,adresse,code_postal,commune,latitude,longitude,telephone,email,site_internet,code_qualification,nom_qualification,url_qualification,nom_certificat,domaine,meta_domaine,organisme,particulier,_file.content,_file.content_type,_file.content_length,_attachment_url,_geopoint,_id,_i,_rand&format=json&q=12345678900000&q_mode=simple
   end
 
-  def historique_rge(params)
-    uri = historique_rge_uri(params)
+  # Use params to build the URI or force URI (for direct next pages)
+  # rubocop:disable Metrics/AbcSize
+  def historique_rge(params) # rubocop:disable Metrics/MethodLength
+    uri ||= params[:uri] || historique_rge_uri(params)
     body = Net::HTTP.get(URI(uri))
 
     raise ServiceUnavailableError if body.include?("all shards failed")
     raise ServiceUnavailableError, uri if body.include?("Impossible d'effectuer cette")
 
-    JSON.parse(body)
+    json = JSON.parse(body)
+    return json if !json.key?("next") || json["next"].blank?
+
+    next_json = historique_rge(uri: json.fetch("next"))
+    json.merge(
+      "next" => next_json["next"],
+      "results" => json["results"] + next_json.fetch("results")
+    )
   end
+  # rubocop:enable Metrics/AbcSize
 end
