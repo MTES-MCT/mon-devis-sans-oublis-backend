bc19e7dccce7c5bf54e46b573a703a0e558be3f1
nicolasleger <nicolasleger@users.noreply.github.com>
2025-10-07 17:42:37 +0200

    feat: more Matomo stats

--- lib/matomo_api.rb
+++ lib/matomo_api.rb
@@ -2,10 +2,14 @@
 
 require "net/http"
 
+require "faraday"
+
 # See https://developer.matomo.org/api-reference/reporting-api#standard-api-parameters
 class MatomoApi
   class ResultError < StandardError; end
   class TimeoutError < ResultError; end
+
+  attr_reader :domain, :id_site
 
   def initialize(domain: nil, id_site: nil, token_auth: nil)
     @domain = domain || ENV.fetch("MATOMO_DOMAIN", "stats.beta.gouv.fr")
@@ -21,20 +25,85 @@ class MatomoApi
   def value(method: "VisitsSummary.getUniqueVisitors", period: "range", date: nil)
     date ||= "2011-01-01,#{1.day.from_now.strftime('%Y-%m-%d')}"
 
-    response = Net::HTTP.post_form(URI("https://#{@domain}/index.php"), {
-                                     module: "API",
-                                     format: "JSON",
-                                     idSite: @id_site,
-                                     method:,
-                                     period:,
-                                     date:,
-                                     token_auth: @token_auth
-                                   })
+    path_params = {
+      module: "API",
+      format: "JSON",
+      idSite: id_site,
+      method:,
+      date:,
+      period:
+    }
+    uri = "#{base_url}?#{URI.encode_www_form(path_params)}"
+    response = wrap_timeout { connection.post(uri, token_auth: @token_auth) }
 
-    json = JSON.parse(response.body)
+    json = parse_response(response)
     json.fetch("value")
-  rescue Net::ReadTimeout => e
-    raise TimeoutError, e
   end
+
   # rubocop:enable Metrics/MethodLength
+  def get_page_url(page_url, date: "today", period: "day") # rubocop:disable Metrics/MethodLength
+    path_params = {
+      module: "API",
+      format: "JSON",
+      idSite: id_site,
+      method: "Actions.getPageUrl",
+      pageUrl: page_url,
+      date: date,
+      period: period
+    }
+
+    uri = "#{base_url}?#{URI.encode_www_form(path_params)}"
+    response = connection.post(uri, token_auth: @token_auth)
+
+    parse_response(response)
+  end
+
+  def get_page_urls(date: "today", period: "day") # rubocop:disable Metrics/MethodLength
+    path_params = {
+      module: "API",
+      format: "JSON",
+      idSite: id_site,
+      method: "Actions.getPageUrls",
+      date: date,
+      period: period
+    }
+
+    uri = "#{base_url}?#{URI.encode_www_form(path_params)}"
+    response = connection.post(uri, token_auth: @token_auth)
+
+    parse_response(response)
+  end
+
+  private
+
+  def base_url
+    "https://#{@domain}/index.php"
+  end
+
+  def connection
+    @connection ||= Faraday.new(url: base_url) do |f|
+      f.request :url_encoded
+      f.adapter Faraday.default_adapter
+
+      f.ssl.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+  end
+
+  def parse_response(response)
+    json = JSON.parse(response.body) if response.success?
+    if json.nil? ||
+       (json.is_a?(Hash) && json["result"] == "error")
+      raise ResultError, "HTTP #{response.status} #{response.body}"
+    end
+
+    json
+  end
+
+  def wrap_timeout
+    yield
+  rescue Faraday::TimeoutError,
+         Faraday::ConnectionFailed,
+         Net::ReadTimeout => e
+    raise TimeoutError, e.message
+  end
 end
